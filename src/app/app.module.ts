5fe951665c9743d85bcfe3cd8c05b52a225870cf
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-23 02:32:23 +0200

    Generate tasks-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InsertTaskComponent } from './insert-task/insert-task.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InsertTaskComponent
+    InsertTaskComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
